2dfbedecb85291d48e15aec361aa9f22bd8f51cd
Charlie0215 <dlf772@gmail.com>
2019-01-06 13:48:43 -0500

    update

--- gpio.py
+++ gpio.py
@@ -1,13 +1,77 @@
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import sys
 import time
+import threading
 
-pin_camera_1 = 17
-pin_camera_2 = 18
-pin_camera_3 = 27
-pin_camera_4 = 22
+pin_for_camera_1 = 17
+pin_for_camera_2 = 18
+#pin_camera_3 = 27
+#pin_camera_4 = 22
 
+lock = threading.Lock()
 
+class  vibrate_thread(threading.Thread):  
+    def __init__(self, threadID, name, counter, pin):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        if pin == 1:
+            self.pin = pin_for_camera_1
+        else:
+            self.pin = pin_for_camera_2
+    def run(self):
+        global n, lock
+        lock.acquire()
+        print("Starting " + self.name)
+        vibrate(self.pin)
+        print("Exiting " + self.name)
+        exit(self.pin)
+        lock.release()
+def vibrate(pin):
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(pin,GPIO.OUT)
+	GPIO.output(pin, GPIO.HIGH)
+	time.sleep(0.25)
+	#GPIO.output(pin, GPIO.LOW)
+def exit(pin):
+	GPIO.output(pin, GPIO.LOW)
+	print("Stop")
+
+'''
+class camera(threading.Thread):
+	def __init__(self, pin):
+		threading.Thread.__init__(self)
+		if pin == 1:
+			self.pin = pin_for_camera_1
+		else:
+			self.pin = pin_for_camera_2
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(self.pin,GPIO.OUT)
+		GPIO.output(self.pin,GPIO.LOW)
+		#self.threads = []
+
+	def vibrate(self):
+		GPIO.output(self.pin, GPIO.HIGH)
+		time.sleep(0.6)
+		GPIO.output(self.pin, GPIO.LOW)
+		#time.sleep(0.1)
+		#GPIO.cleanup()
+
+	def off(self):
+		GPIO.output(self.pin, GPIO.LOW)
+		#GPIO.cleanup()
+		print("Stop")
+	
+	def thread(self):
+		threads = []
+		t = threading.Thread(target=self.vibrate)
+		threads.append(t)
+	
+'''
+'''
 class camera():
 	def __init__(self):
 		self.pin = pin_camera_1
@@ -51,3 +115,5 @@ def dim_led():
 		pass
 	p.stop()
 	GPIO.cleanup()
+'''
+	
